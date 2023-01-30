3b872a20d1204096ebadb7de5c88b412bfda8d5f
Callum Nightingale <callum.nightingale@capgemini.com>
2023-01-30 20:38:41 +0000

    refactor: update logo to header

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,7 +12,7 @@ const NavBar = (props) => {
     <NavBarContainer {...props}>
       <Logo
         w="100px"
-        color={["white", "white", "primary.500", "primary.500"]}
+        color="white"
       />
       <MenuToggle toggle={toggle} isOpen={isOpen} />
       <MenuLinks isOpen={isOpen} />
