4afc6e6997e5f1791b02ad920ee7f44f1dfcead9
Callum Nightingale <callum.nightingale@capgemini.com>
2023-06-05 10:56:09 +0100

    feat: add blog

---
+++ src/const/blogPosts.js
@@ -0,0 +1,12 @@
+const blogPosts = [
+    {
+        "title": "The GUI is go!",
+        "body": "We can finally announce that our application finally has a GUI so all our users can experience a better playing journey."
+    },
+    {
+        "title": "Cards on the table!",
+        "body": "It's official - our new special cards are in the run to be complete very soon for all players to be able to play these cards whenever they choose."
+    }
+];
+
+export default blogPosts;
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,9 +1,55 @@
+import {
+  Box,
+  Heading,
+  StackDivider,
+  VStack,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Text,
+} from "@chakra-ui/react";
 import DefaultContainer from "../containers/DefaultContainer";
+import blogPosts from "../const/blogPosts";
 
 const Blog = () => {
-    return (
-        <DefaultContainer>Blog</DefaultContainer>
-    );
-}
+  return (
+    <DefaultContainer>
+      <VStack
+        divider={<StackDivider />}
+        spacing={4}
+        align="stretch"
+        maxW="80ch"
+        color="white"
+      >
+        <Box>
+          <Heading pb={2} pl={3}>
+            Blog
+          </Heading>
+        </Box>
+        {blogPosts.map((post) => (
+          <Card
+            align="center"
+            overflow="hidden"
+            variant="outline"
+            p="10px"
+            bg="transparent"
+            color="white"
+            border="none"
+            shadow="dark-lg"
+            w={{ base: "100%", sm: "400px" }}
+          >
+            <CardHeader>
+              <Heading size="md">{post.title}</Heading>
+            </CardHeader>
+            <CardBody>
+              <Text>{post.body}</Text>
+            </CardBody>
+          </Card>
+        ))}
+      </VStack>
+    </DefaultContainer>
+  );
+};
 
 export default Blog;
